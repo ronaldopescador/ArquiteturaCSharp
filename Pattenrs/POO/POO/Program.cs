3d74e13d43d4b40411845f28045281d387a32bfc
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-05-11 20:24:14 -0300

    .

---
+++ Pattenrs/POO/POO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Calculadora
+{
+    public double Num1 { get; set; }
+    public double Num2 { get; set; }
+    public Calculadora(double num1, double num2)
+    {
+        Num1 = num1;
+        Num2 = num2;
+    }
+    public double Somar()
+    {
+        return Num1 + Num2;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora(10, 35);
+
+        double resultado = calc.Somar();
+
+        Console.WriteLine("O resultado da soma é: " + resultado);
+    }
+}
