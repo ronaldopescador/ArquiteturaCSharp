3c3232bcd2b7a8d2ff5f8cec3bd9cef78e273e54
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-05-11 20:17:05 -0300

    .

---
+++ Pattenrs/Calculadora_PE/Calculadora_PE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double resultado = Somar(10, 35);
+    }
+
+    static double Somar(double a, double b)
+    {
+        return a + b;
+    }
+}
+
