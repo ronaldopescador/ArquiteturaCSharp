625081d6d93fd000f255dbe515f7420cbd3bc5c4
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-05-16 19:45:23 -0300

    .

---
+++ Pattenrs/Exemplo01_Singleton/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo01_Singleton
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        private Logger() { }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Logger();
+            return _instance;
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine($"[LOG]: {message}");
+        }
+    }
+}
---
+++ Pattenrs/Exemplo01_Singleton/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo01_Singleton
+{
+    public class Pedido
+    {       
+        public void Incluir(int id)
+        {
+            Logger.GetInstance().Log($"Pedido {id} incluído");
+        }
+    }
+}
